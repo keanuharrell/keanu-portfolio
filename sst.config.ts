0c1ab008fbe386a5ae33ac3d4ef30f9fe18e6b45
Keanu HARRELL <keanuharrell@icloud.com>
2025-09-28 22:07:06 +0200

    Réactiver la gestion de la suppression et de la protection dans la configuration de SST

--- sst.config.ts
+++ sst.config.ts
@@ -3,12 +3,13 @@ export default $config({
   app(input) {
     return {
       name: "keanu-portfolio",
-      // removal: input?.stage === "production" ? "retain" : "remove",
-      // protect: ["production"].includes(input?.stage),
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
       home: "aws",
       providers: {
         aws: {
           profile: "sst",
+          region: "eu-west-3",
         },
         cloudflare: {
           version: "6.3.1",
